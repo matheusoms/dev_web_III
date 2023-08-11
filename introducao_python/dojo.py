39cd1e2814e80f94dfa955d370aaae893ea874f3
Matheus Matias <111811840+matheusoms@users.noreply.github.com>
2023-08-10 22:09:13 -0300

    dojo

---
+++ introducao_python/dojo.py
@@ -0,0 +1,13 @@
+def fizz_buzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+
+for i in range(101):
+    result = fizz_buzz(i)
+    print(result)
