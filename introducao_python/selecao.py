12725774e944f8d01ee6576f5cd48beb4f75a2f2
Matheus Matias <111811840+matheusoms@users.noreply.github.com>
2023-08-10 21:45:30 -0300

    Primeira aula de Python

---
+++ introducao_python/selecao.py
@@ -0,0 +1,6 @@
+idade = 20
+
+if idade > 18:
+    print('Pode beber')
+elif idade <= 17:
+    print('Não pode beber')
---
+++ introducao_python/sequencia.py
@@ -0,0 +1,13 @@
+def passos():
+    print('Primeiro passo')
+    print('Segundo passo')
+    print('Terceiro passo')
+
+passos()
+
+def imprima_seu_nome():
+    print('Qual o seu nome?')
+    nome = input('Insira seu nome: ')
+    print(nome)
+
+imprima_seu_nome()
